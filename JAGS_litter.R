77504cfd2dd566dde1e93a1efac976786958080e
will2vos <129417301+will2vos@users.noreply.github.com>
2023-11-22 15:27:28 -0500

    modification du document de rencontre

--- JAGS_litter.R
+++ JAGS_litter.R
@@ -1,6 +1,6 @@
 #cut ~ litter
 #positionnement
-setwd(dir = "/Users/williamdevos/Documents/Maitrise/Data")
+# setwd(dir = "/Users/williamdevos/Documents/Maitrise/Data")
 
 #dataset
 load("SEM_data_JAGS.RData")
@@ -13,7 +13,7 @@ Litter <- data.JAGS$Obs_cov$litter.depth
 Coupe <- data.JAGS$Obs_cov$Coupe
 
 ##numeric ID to select groups
-CoupeNum <- as.numeric(data.JAGS$Obs_cov$Coupe)
+#CoupeNum <- as.numeric(data.JAGS$Obs_cov$Coupe)
 #3=témoin, 1=partielle, 2=totale
 
 BlockOrig <- data.JAGS$Obs_cov$Bloc
@@ -47,32 +47,32 @@ for(m in 1:nblocks) {
 tau.block <- pow(sigma.block, -2)
 sigma.block ~ dunif(0, 10)
 
-##different variance for each group
-for(j in 1:3) {
-  tau.lit[j] <- pow(sigma[j], -2)
-  sigma[j] ~ dunif(0, 150)
-}
+
+##prior for residual variance
+sigma ~ dunif(0, 10)
+tau.lit <- 1/(sigma * sigma)
 
 
   ## Likelihood
   for (i in 1:nsites) {
      mu.lit[i] <- beta0 + beta.Cutpartial*Cutpartial[i] + beta.Cutclear*Cutclear[i] + alpha.block[Block[i]]
 
-     Litter[i] ~ dnorm(mu.lit[i], tau.lit[CoupeNum[i]])
+     Litter[i] ~ dnorm(mu.lit[i], tau.lit)
   }
 
-##predicted values
-for(i in 1:nsites) {
-pred[i] <- mu.lit[i]
+## Derived values
+for (i in 1:nsites) {
+   res[i] <- Litter[i] - mu.lit[i]
+   pred[i] <- mu.lit[i] #linear predictor
+}
 
-##raw residuals
-res[i] <- Litter[i] - mu.lit[i]
-##Pearson residuals
-res.pearson[i] <- res[i]/sigma[CoupeNum[i]]
+## Derived values
+#  res.pearson[i] <- res[i]/sigma[i]
+
 
 }
 
-}
+
 "
 writeLines(modelstring, con = "Litter_Cut-hetVar-block.jags")
 
@@ -84,12 +84,13 @@ lin.data <- list(
   Litter = as.numeric(Litter),
   Cutpartial = as.numeric(Cutpartial),
   Cutclear = as.numeric(Cutclear),
-  CoupeNum = CoupeNum,
   nsites = nsites,
   Block = Block,
   nblocks = 4
 )
 str(lin.data)
+
+# CoupeNum = CoupeNum,
 
 
 # initial values of intercepts
@@ -98,7 +99,7 @@ inits <- function() {
     beta0 = rnorm(1),
     beta.Cutpartial = rnorm(1),
     beta.Cutclear = rnorm(1),
-    sigma = rlnorm(3),
+    sigma = rlnorm(1),
     alpha.block = rnorm(nblocks),
     sigma.block = runif(1, 0, 10)
   )
@@ -113,7 +114,6 @@ params <- c(
   "sigma",
   "pred",
   "res",
-  "res.pearson",
   "sigma.block"
 )
 
@@ -151,16 +151,14 @@ print(out.lit, digits = 3)
 out.lit$summary[c("beta0",
                   "beta.Cutpartial",
                   "beta.Cutclear",
-                  "sigma[1]",
-                  "sigma[2]",
-                  "sigma[3]"),
+                  "sigma"),
                 c("mean", "sd", "2.5%", "97.5%", "Rhat")]
 
 ##Rhat
 hist(out.lit$summary[, "Rhat"])
 any(out.lit$summary[, "Rhat"] > 1.1)
 
-par(mfrow = c(2, 3),
+par(mfrow = c(2, 2),
     mar = c(4, 4, 2, 2))
 
 matplot(
@@ -207,46 +205,18 @@ matplot(
 
 matplot(
   cbind(
-    out.lit$samples[[1]][, "sigma[1]"],
-    out.lit$samples[[2]][, "sigma[1]"],
-    out.lit$samples[[3]][, "sigma[1]"],
-    out.lit$samples[[4]][, "sigma[1]"],
-    out.lit$samples[[5]][, "sigma[1]"]
+    out.lit$samples[[1]][, "sigma"],
+    out.lit$samples[[2]][, "sigma"],
+    out.lit$samples[[3]][, "sigma"],
+    out.lit$samples[[4]][, "sigma"],
+    out.lit$samples[[5]][, "sigma"]
   ),
   type = "l",
-  ylab = "sigma[1]",
+  ylab = "sigma",
   xlab = "iteration",
   cex.lab = 1.2
 )
 
-matplot(
-  cbind(
-    out.lit$samples[[1]][, "sigma[2]"],
-    out.lit$samples[[2]][, "sigma[2]"],
-    out.lit$samples[[3]][, "sigma[2]"],
-    out.lit$samples[[4]][, "sigma[2]"],
-    out.lit$samples[[5]][, "sigma[2]"]
-  ),
-  type = "l",
-  ylab = "sigma[2]",
-  xlab = "iteration",
-  cex.lab = 1.2
-)
-
-
-matplot(
-  cbind(
-    out.lit$samples[[1]][, "sigma[3]"],
-    out.lit$samples[[2]][, "sigma[3]"],
-    out.lit$samples[[3]][, "sigma[3]"],
-    out.lit$samples[[4]][, "sigma[3]"],
-    out.lit$samples[[5]][, "sigma[3]"]
-  ),
-  type = "l",
-  ylab = "sigma[3]",
-  xlab = "iteration",
-  cex.lab = 1.2
-)
 
 
 ##using coda package for additional diagnostics
@@ -271,3 +241,117 @@ range(coda.out$statistics[, "Time-series SE"] / coda.out$statistics[, "SD"])
 outSum.lit <-
   out.lit$summary [, c("mean", "sd", "2.5%", "97.5%", "Rhat")]
 outSum.lit
+
+
+
+
+#```{r, echo=FALSE}
+
+# sample data
+lit_data <- data.frame(
+  treatments = c("Témoin", "Partielle", "Totale"),
+  mean = c(outSum.lit[1:3, 1]),
+  lower = c(outSum.lit[1:3, 3]),
+  upper = c(outSum.lit[1:3, 4])
+)
+
+lit_data
+##graphics to compare groups, IC 95%
+
+par(mfcol = c(1, 1),
+    mar = c(5.1, 5.1, 4.1, 4.1))
+
+diff.1v2.lit <- out.lit$sims.list$beta0 -
+  out.lit$sims.list$beta.Cutpartial
+hist(diff.1v2.lit,
+     main = "Différence des distributions de postérieurs \npour la profondeur de litière",
+     xlab = "Témoin vs coupe partielle",
+     ylab = "Fréquence")
+quant1v2.lit <- quantile(diff.1v2.lit, probs = c(0.025, 0.975))
+abline(
+  v = quant1v2.lit,
+  lty = 2,
+  col = "red",
+  lwd = 3
+)
+quant1v2.lit
+
+diff.1v3.lit <- out.lit$sims.list$beta0 -
+  out.lit$sims.list$beta.Cutclear
+hist(diff.1v3.lit,
+     main = NA,
+     xlab = "Témoin vs coupe totale",
+     ylab = "Fréquence")
+quant1v3.lit <- quantile(diff.1v3.lit, probs = c(0.025, 0.975))
+abline(
+  v = quant1v3.lit,
+  lty = 2,
+  col = "red",
+  lwd = 3
+)
+quant1v3.lit
+
+diff.2v3.lit <- out.lit$sims.list$beta.Cutpartial -
+  out.lit$sims.list$beta.Cutclear
+hist(diff.2v3.lit,
+     main = NA,
+     xlab = "Coupe partielle vs coupe totale",
+     ylab = "Fréquence")
+quant2v3.lit <- quantile(diff.2v3.lit, probs = c(0.025, 0.975))
+abline(
+  v = quant2v3.lit,
+  lty = 2,
+  col = "red",
+  lwd = 3
+)
+quant2v3.lit
+
+#```
+
+#```{r, echo=FALSE}
+par(mfcol = c(1, 1),
+    mar = c(5.1, 5.1, 4.1, 4.1))
+
+
+# Create a  plot
+plot(
+  NA,
+  xlim = c(0, 4),
+  ylim = c(-5, 010),
+  main = "Profondeur de litière par coupe forestières",
+  xlab = "Coupes forestières",
+  ylab = "Profondeur (cm)",
+  xaxt = "n",
+  cex.axis = 1.2,
+  cex.lab = 1.2,
+  cex.main = 1.5
+)
+
+# Add lines for each treatment
+segments(
+  x0 = 1:3,
+  y0 = lit_data$lower,
+  1:3,
+  lit_data$upper,
+  col = "black",
+  lwd = 2
+)
+
+# Add points for mean occupation probabilities
+points(
+  x = 1:3,
+  y = lit_data$mean,
+  pch = 19,
+  col = "black",
+  lwd = 4
+)
+
+axis(
+  1,
+  at = 1:3,
+  labels = lit_data$treatments,
+  lwd = 1,
+  padj = 0.5
+)
+
+#```
